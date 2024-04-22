eaa4b2ef3733627338e6b2e0a69cb2a04e3d9755
IMESYA <167765086+IMESYA@users.noreply.github.com>
2024-04-22 17:25:17 +0200

    main

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,16 @@
-fx_version "cerulean"
-games { 'gta5' }
+fx_version 'cerulean'
+game 'gta5'
 
 author 'G&N_s Studio'
+<<<<<<< Updated upstream
 description 'Sandy Shore Mapdata - Burgershot+Sheriff+StripMall'
 version '4.0.0'
+=======
+description 'Sandy Shore Mapdata - Clinic Center + Burgershot + Sheriff Dept + Motel + Strip Mall'
+version '3.0.0'
+>>>>>>> Stashed changes
 
-dependencies {
-    '/gameBuild:2189'
-}
+dependency '/gameBuild:2189'
 
 this_is_a_map 'yes'
 
