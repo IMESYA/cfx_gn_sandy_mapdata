c0a5876b2a5f676d356eb90d6db8548789a90f52
IMESYA <167765086+IMESYA@users.noreply.github.com>
2024-04-22 17:12:12 +0200

    Burgershot+Sheriff+StripMall

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,13 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+games { 'gta5' }
 
 author 'G&N_s Studio'
-description 'Sandy Shore Mapdata - Clinic Center + Burgershot + Sheriff Dept + Motel + Strip Mall'
-version '3.0.0'
+description 'Sandy Shore Mapdata - Burgershot+Sheriff+StripMall'
+version '4.0.0'
 
-dependency '/gameBuild:2189'
+dependencies {
+    '/gameBuild:2189'
+}
 
 this_is_a_map 'yes'
 
