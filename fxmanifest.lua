6d345cda9cc30414545980f2fdcb9068bde5833d
IMESYA <167765086+IMESYA@users.noreply.github.com>
2024-04-24 13:14:40 +0200

    Sheriff + Burgershot + Stripmall

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 games { 'gta5' }
 
 author 'IMESYA'
-description 'Sandy Shore Mapdata - Burgershot + Sheriff + StripMall'
+description 'Sandy Shore Mapdata - Sheriff + Burgershot + Stripmall'
 version '4.0.0'
 
 dependencies {
